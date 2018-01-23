6d4559fea18aa7cb26bc8151c1d21c062831fc33
admarlo <2dam.ademartin@institutoserpis.org>
2018-01-23 11:37:54 +0100

    Añadido ListaTarea2

--- ListaTarea2/app/src/main/java/com/example/adrlop/listatarea2/DataBaseHelper.java
+++ ListaTarea2/app/src/main/java/com/example/adrlop/listatarea2/DataBaseHelper.java
@@ -1,4 +1,4 @@
-package com.acme.todolist;
+package com.example.adrlop.listatarea2;
 
 import android.content.ContentValues;
 import android.content.Context;
--- ListaTarea2/app/src/main/java/com/example/adrlop/listatarea2/ItemActivity.java
+++ ListaTarea2/app/src/main/java/com/example/adrlop/listatarea2/ItemActivity.java
@@ -1,4 +1,4 @@
-package com.acme.todolist;
+package com.example.adrlop.listatarea2;
 
 import android.app.Activity;
 import android.content.Context;
